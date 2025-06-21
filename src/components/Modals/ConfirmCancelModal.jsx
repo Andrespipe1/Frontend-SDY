44b06782acf2e4445b44e532c8d7a26588c526ea
Andres <andresftma@gmail.com>
2025-06-21 18:51:31 -0500

    Boton arreglado

--- src/components/Modals/ConfirmCancelModal.jsx
+++ src/components/Modals/ConfirmCancelModal.jsx
@@ -28,7 +28,7 @@ const ConfirmCancelModal = ({ isOpen, onClose, onConfirm, message }) => {
             onClick={onConfirm}
             className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
           >
-            Eliminar
+            Confirmar
           </button>
         </div>
       </div>
